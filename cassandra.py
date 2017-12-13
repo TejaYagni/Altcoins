2db273c106eaba095f51a64ce62ec4060c499efa
Harvey <saitejayagni@gmail.com>
2017-12-12 20:04:07 -0600

    made changes to cassandra.py

--- cassandra.py
+++ cassandra.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 cluster = Cluster()
 session = cluster.connect()
-create_keyspace = "CREATE KEYSPACE IF NOT EXISTS altcoins" \
+create_keyspace = "CREATE KEYSPACE IF NOT EXISTS altcoins_keyspace" \
                   " WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};"
 create_type_inputs = "CREATE TYPE IF NOT EXISTS inputs_type("\
                     "previous_txid varchar"\
@@ -17,7 +17,7 @@ create_type_outputs = "CREATE TYPE IF NOT EXISTS outputs_type("\
                     "script varchar"\
                     "output_no int;"
 
-create_type_date = "CREATE TYPE IF NOT EXISTS data ("\
+create_type_data = "CREATE TYPE IF NOT EXISTS data_type ("\
                     "txid varchar,"\
                     "confirmations boolean,"\
                     "block_hash varchar"\
@@ -29,32 +29,19 @@ create_type_date = "CREATE TYPE IF NOT EXISTS data ("\
                     "is_green boolean"\
                     "inputs set<frozen<inputs_type>>"\
                     "outputs set<frozen<outputs_type>>"
-create_table_transport = """
+create_table_altcoins = """
 
 CREATE TABLE altcoins(
-   head_rt_version double PRIMARY KEY,
-   head_time_stamp timestamp,  
-   head_user_data varchar,
-   id varchar,
-   is_deleted Boolean,
-   trip_id bigint,
-   route_id int,
-   start_date date,
-   schedule_relationship int,
-   vehicle_id int,
-   vehicle_label int,
-   latitude double,
-   longitude double,
-   bearing int,
-   odometer float,
-   speed float,
-   current_stop_sequence int,
-   current_status int,
-   vehicle_time_stamp timestamp,
-   vehicle_congestion_level int
+   txid varchar,
+   type varchar,
+   data data_type,
+   PRIMARY KEY TXID
    )
 
 """
 print(session.execute(create_keyspace))
-print(session.execute("use Transport_keyspace"))
-print(session.execute(create_table_transport))
+print(session.execute("use altcoins_keyspace"))
+print(session.execute(create_type_inputs))
+print(session.execute(create_type_outputs))
+print(session.execute(create_type_data))
+print(session.execute(create_table_altcoins))

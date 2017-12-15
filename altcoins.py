e98bf33ab392754b45128a99c79f11d42af3db29
Harvey <saitejayagni@gmail.com>
2017-12-15 16:43:53 -0600

    changes the schema for cassandra

--- altcoins.py
+++ altcoins.py
@@ -9,6 +9,12 @@ import time
 import json
 
 
+from cassandra.cluster import Cluster
+cluster = Cluster()
+session = cluster.connect("altcoins_keyspace")
+
+
+
 from satori.rtm.client import make_client, SubscriptionMode
 
 endpoint = "wss://open-data.api.satori.com"
@@ -16,15 +22,36 @@ channel = "altcoins"
 
 def main():
     with make_client(endpoint=endpoint, appkey=application_key.appkey) as client:
+        session.execute("INSERT INTO abc(val, val2) values (2,'king');")
         print('Connected to Satori RTM!')
 
         class SubscriptionObserver(object):
             def on_subscription_data(self, data):
                 for message in data['messages']:
                     print("Got message:", message['data']['network'])
-                    with open("Data/"+message['data']['network']+message['data']['txid']+".json",'w') as f:
-                        json.dump(message, f)
-
+                    '''
+                    session.execute("INSERT INTO altcoins("
+                                    " txid, "
+                                    " data.network, "
+                                    " data.txid, "
+                                    " data.received_at, "
+                                    " data.network_fee, "
+                                    " data.amount_received, "
+                                    " data.inputs, "
+                                    " data.outputs ) "
+                                    "values "
+                                    "( "
+                                    +message['data']['txid'] + \
+                                    +message['data']['network'] \
+                                    +message['data']['txid'] \
+                                    +message['data']['received_at'] \
+                                    +message['data']['network_fee'] \
+                                    +message['data']['amount_received'] \
+                                    +message['data']['inputs'] \
+                                    +message['data']['outputs']
+                                    +");"
+                                    )
+'''
 
         subscription_observer = SubscriptionObserver()
         client.subscribe(
--- create_cassandara_table.py
+++ create_cassandara_table.py
@@ -3,32 +3,47 @@ cluster = Cluster()
 session = cluster.connect()
 create_keyspace = "CREATE KEYSPACE IF NOT EXISTS altcoins_keyspace" \
                   " WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};"
-create_type_inputs = "CREATE TYPE IF NOT EXISTS inputs_type("\
-                    "previous_txid varchar, "\
-                    "type varchar, "\
-                    "address varchar, "\
-                    "amount float, "\
-                    "script varchar, "\
-                    "input_no int) ;"
-create_type_outputs = "CREATE TYPE IF NOT EXISTS outputs_type("\
-                    "type varchar, "\
-                    "address varchar, "\
-                    "amount float, "\
-                    "script varchar, "\
-                    "output_no int);"
+create_type_inputs = """
+CREATE TYPE IF NOT EXISTS altcoins_keyspace.inputs_type (
+    previous_txid text,
+    type text,
+    address text,
+    amount float,
+    script text,
+    input_no int,
+    previous_output_no int,
+    confirmed boolean,
+    confirmations int,
+    unconfirmedinput boolean,
+    doublespenttxid bigint
+);
+"""
+create_type_outputs = """
+CREATE TYPE IF NOT EXISTS altcoins_keyspace.outputs_type (
+    type text,
+    address text,
+    amount float,
+    script text,
+    output_no int
+);
+"""
 
-create_type_data = "CREATE TYPE IF NOT EXISTS data_type ("\
-                    "txid varchar,"\
-                    "confirmations boolean,"\
-                    "block_hash varchar,"\
-                    "block_no int, "\
-                    "block_time timestamp, "\
-                    "received_at timestamp, "\
-                    "network_fee float, "\
-                    "amount_received float, "\
-                    "is_green boolean, "\
-                    "inputs set<frozen<inputs_type>>, "\
-                    "outputs set<frozen<outputs_type>>);"
+create_type_data = """
+CREATE TYPE altcoins_keyspace.data_type (
+    txid text,
+    confirmations int,
+    block_hash text,
+    block_no int,
+    block_time bigint,
+    received_at bigint,
+    network_fee float,
+    amount_received float,
+    is_green boolean,
+    inputs set<frozen<inputs_type>>,
+    outputs set<frozen<outputs_type>>,
+    network text
+);
+"""
 create_table_altcoins = """
 
 CREATE TABLE IF NOT EXISTS altcoins(

c90eb114d1a2802486a74db188b959d389c490c7
Harvey <saitejayagni@gmail.com>
2017-12-26 13:46:39 -0600

    modififed to script, kafka setup and cassandra file

--- kafka-setup.sh
+++ kafka-setup.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-#Move to zookeeper directory and then use this command
-sudo su
-cd
-cd zookeeper-3.3.6
-bin/zkServer.sh start > /tmp/zookeeper.logs &
-
-#Move to Kafka directory and then use this command
-cd
-cd kafka_2.12-0.11.0.2/
+# start zookeeper
+ /home/harvee/zookeeper-3.3.6/bin/zkServer.sh start > /tmp/zookeeper.logs &
 
 #Start the kafka server
-bin/kafka-server-start.sh config/server.properties > tmp/kafka_server.logs &
+ /home/harvee/kafka_2.12-0.11.0.2/bin/kafka-server-start.sh /home/harvee/kafka_2.12-0.11.0.2/config/server.properties > /tmp/kafka_server.logs &
 
 # Create a kafka topic
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic altcoins
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic altcoins2
+ /home/harvee/kafka_2.12-0.11.0.2/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic altcoins
+ /home/harvee/kafka_2.12-0.11.0.2/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic altcoins2

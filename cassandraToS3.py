4abec27514bf9d184add3b6d642c5da5e2cea71d
Harvey <saitejayagni@gmail.com>
2017-12-26 10:55:09 -0600

    changes made to schema

--- cassandraToS3.py
+++ cassandraToS3.py
@@ -8,9 +8,7 @@ cluster = Cluster()
 session = cluster.connect("altcoins_keyspace")
 
 date_collected = \
-    datetime.datetime.fromtimestamp(
-                                    int("1284101485")
-                                    ).strftime('%Y-%m-%d')
+    datetime.datetime.today().strftime('%Y-%m-%d')
 
 
 def main():
@@ -19,11 +17,10 @@ def main():
     results = session.execute("select * from altcoins;")
     with open("Data/"+file_name,'w') as f:
         for result in results:
-            #print result.data.txid,result.type
-            f.write(result)
+            f.write(str(result))
             #f.write(str(result.data.txid)+","+str(result.data.received_at)+","+str(result.data.network_fee)+","+str(result.data.amount_received)+","+str(result.type))
     obj = s3.Object(bucket_name, path + file_name)
-    obj.put(Body=open(file_name,'rb'))
+    obj.put(Body=open("Data/"+file_name,'rb'))
 
 if __name__ == '__main__':
     main()
